59a333696af08a89381929033ac563dc2a218a00
risang443 <risang443@gmail.com>
2023-11-02 20:20:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "text-prime": "#18191A",
+        "text-second": "#FEB913",
+        "button-prime": "#D80000",
+        "button-second": "#FEB913",
+      },
+      fontFamily: {
+        pop: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
